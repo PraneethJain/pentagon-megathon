123a4fbcf063f4c9d24f631552b274095379ebb3
Dikshant <dikshant.073@gmail.com>
2023-10-29 04:18:25 +0530

    cloned app from frontend repo

---
+++ READMEforAndroid.md
@@ -0,0 +1,16 @@
+# To Do:
+- [x] App Logo
+- [x] UI interface
+- [x] Welcome Page, Registration Page
+- [x] Push data to Realtime firebase
+- [ ] Questions
+- [ ] Score Calculation
+
+# Purpose:
+We built an app, that can be used for **personality based classification** and qualification criteria for job interviews. The final results are 
+calculated in a model trained with ML.
+This app has a basic **puzzle based test**, which is used for personality classification. The output of this test is sent to the database, where 
+other phases of this **virtual interview**, based on **social media analysis** is calculated.
+
+# Overview:
+While building this app we've used **Android Studio** platform in **Kotlin** language.
---
+++ frontend/app/PsycheTest/app/src/main/java/com/example/psychetest/RegistrationActivity.kt
@@ -0,0 +1,49 @@
+package com.example.psychetest
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.psychetest.databinding.ActivityRegistrationBinding
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+
+class RegistrationActivity : AppCompatActivity() {
+    private var binding: ActivityRegistrationBinding? = null
+    private lateinit var database : DatabaseReference
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        binding?.btnSignUp?.setOnClickListener {
+            val Name = binding?.etSinUpName?.text.toString()
+            val Email = binding?.etSinUpEmail?.text.toString()
+            val Linkedin = binding?.etSinUpLinkedIn?.text.toString()
+            val Instagram = binding?.etSinUpInstagram?.text.toString()
+            val Twitter = binding?.etSinUpTwitter?.text.toString()
+
+//            database = FirebaseDatabase.getInstance().getReference("Users")
+            database = FirebaseDatabase.getInstance().reference.child("Users")
+            val userKey = database.push().key //to be removed
+//            val User = User(Name,Email,Linkedin, Instagram, Twitter)
+            val user = User(Name, Email, Linkedin, Instagram, Twitter)
+            database.child(userKey!!).setValue(user).addOnSuccessListener{
+//            database.child(Name).setValue(Email).addOnSuccessListener {
+                binding?.etSinUpName?.text?.clear()
+                binding?.etSinUpEmail?.text?.clear()
+                binding?.etSinUpInstagram?.text?.clear()
+                binding?.etSinUpLinkedIn?.text?.clear()
+                binding?.etSinUpTwitter?.text?.clear()
+
+                Toast.makeText(this, "Registered User",Toast.LENGTH_SHORT).show()
+            }.addOnFailureListener {
+                Toast.makeText(this, "Try Again",Toast.LENGTH_SHORT).show()
+            }
+
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
