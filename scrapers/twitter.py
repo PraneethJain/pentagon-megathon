5da691e156c60bb325e600cc54f773ee032adf5b
PraneethJain <praneethjain005@gmail.com>
2023-10-29 02:37:02 +0530

    twitter scraper done

--- scrapers/twitter.py
+++ scrapers/twitter.py
@@ -24,42 +24,12 @@ time.sleep(5)
 driver.get(URL)
 time.sleep(5)
 
+texts = set()
 for _ in range(5):
     driver.find_element(By.TAG_NAME, "html").send_keys(Keys.END)
     time.sleep(2)
+    res = driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="tweetText"]')
+    texts.update(stripped for el in res if (stripped:=el.text.strip()))
 
-raise SystemExit
-
-
-
-
-
-SCROLL_PAUSE_TIME = 1
-driver.find_element(By.CSS_SELECTOR, 'html').send_keys(Keys.END)
-last_height = driver.execute_script("return document.body.scrollHeight")
-while 1:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(SCROLL_PAUSE_TIME)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
-        try:
-            driver.find_element(By.CSS_SELECTOR, "button.artdeco-button.artdeco-button--muted.artdeco-button--1.artdeco-button--full.artdeco-button--secondary.ember-view.scaffold-finite-scroll__load-button").click()
-        except:
-            break
-    last_height = new_height
-
-
-res = driver.find_elements(By.CSS_SELECTOR, 'span[dir="ltr"]')
-num_posts = len(res)
-original_posts = []
-reposts = []
-for i in range(0, num_posts, 2):
-    posted_by = res[i].text.splitlines()[0].strip()
-    if posted_by == profile_name:
-        original_posts.append(res[i + 1].text.strip())
-    else:
-        reposts.append(res[i + 1].text.strip())
-
-print(original_posts)
-print(reposts)
+print(texts)
 driver.close()
