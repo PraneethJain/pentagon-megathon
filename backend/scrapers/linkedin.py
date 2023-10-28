67ca904448aa0ad283fbec370556e455450dd6fa
PraneethJain <praneethjain005@gmail.com>
2023-10-29 03:33:17 +0530

    refactor to functions

--- backend/scrapers/linkedin.py
+++ backend/scrapers/linkedin.py
@@ -6,59 +6,58 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
 
-URL = "https://www.linkedin.com/in/sujitgujar/recent-activity/all/"
 USERNAME = "awesome.harsh8@gmail.com"
 PASSWORD = "harshharsh"
 
-driver = webdriver.Firefox()
-driver.get("https://www.linkedin.com/uas/login")
-wait = WebDriverWait(driver, 30)
-email_field = wait.until(EC.visibility_of_element_located((By.ID, "username")))
-email_field.send_keys(USERNAME)
-pass_field = wait.until(EC.visibility_of_element_located((By.ID, "password")))
-pass_field.send_keys(PASSWORD)
-pass_field.send_keys(Keys.RETURN)
+def linkedin_scraper(URL: str) -> set[str]:
+    driver = webdriver.Firefox()
+    driver.get("https://www.linkedin.com/uas/login")
+    wait = WebDriverWait(driver, 30)
+    email_field = wait.until(EC.visibility_of_element_located((By.ID, "username")))
+    email_field.send_keys(USERNAME)
+    pass_field = wait.until(EC.visibility_of_element_located((By.ID, "password")))
+    pass_field.send_keys(PASSWORD)
+    pass_field.send_keys(Keys.RETURN)
 
-time.sleep(5)
-driver.get(URL)
-time.sleep(5)
+    time.sleep(5)
+    driver.get(URL)
+    time.sleep(5)
 
-profile_name = driver.find_element(
-    By.CSS_SELECTOR, ".single-line-truncate.t-16.t-black.t-bold.mt2"
-).text.strip()
-profile_job_title = driver.find_element(
-    By.CSS_SELECTOR, ".t-14.t-black--light.t-normal.mb1"
-).text.strip()
+    profile_name = driver.find_element(
+        By.CSS_SELECTOR, ".single-line-truncate.t-16.t-black.t-bold.mt2"
+    ).text.strip()
+    profile_job_title = driver.find_element(
+        By.CSS_SELECTOR, ".t-14.t-black--light.t-normal.mb1"
+    ).text.strip()
 
-SCROLL_PAUSE_TIME = 1
-driver.find_element(By.CSS_SELECTOR, 'html').send_keys(Keys.END)
-last_height = driver.execute_script("return document.body.scrollHeight")
-while 1:
-    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(SCROLL_PAUSE_TIME)
-    new_height = driver.execute_script("return document.body.scrollHeight")
-    if new_height == last_height:
+    SCROLL_PAUSE_TIME = 1
+    driver.find_element(By.CSS_SELECTOR, 'html').send_keys(Keys.END)
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    while 1:
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        time.sleep(SCROLL_PAUSE_TIME)
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            try:
+                driver.find_element(By.CSS_SELECTOR, "button.artdeco-button.artdeco-button--muted.artdeco-button--1.artdeco-button--full.artdeco-button--secondary.ember-view.scaffold-finite-scroll__load-button").click()
+            except:
+                break
+        last_height = new_height
+
+
+    res = driver.find_elements(By.CSS_SELECTOR, 'span[dir="ltr"]')
+    num_posts = len(res)
+    original_posts = []
+    reposts = []
+    for i in range(0, num_posts, 2):
+        posted_by = res[i].text.splitlines()[0].strip()
         try:
-            driver.find_element(By.CSS_SELECTOR, "button.artdeco-button.artdeco-button--muted.artdeco-button--1.artdeco-button--full.artdeco-button--secondary.ember-view.scaffold-finite-scroll__load-button").click()
-        except:
-            break
-    last_height = new_height
+            if posted_by == profile_name:
+                original_posts.append(res[i + 1].text.strip())
+            else:
+                reposts.append(res[i + 1].text.strip())
+        except IndexError:
+            continue
 
-
-res = driver.find_elements(By.CSS_SELECTOR, 'span[dir="ltr"]')
-num_posts = len(res)
-original_posts = []
-reposts = []
-for i in range(0, num_posts, 2):
-    posted_by = res[i].text.splitlines()[0].strip()
-    try:
-        if posted_by == profile_name:
-            original_posts.append(res[i + 1].text.strip())
-        else:
-            reposts.append(res[i + 1].text.strip())
-    except IndexError:
-        continue
-
-print(original_posts)
-print(reposts)
-driver.close()
+    driver.close()
+    return set(original_posts + reposts)
--- backend/scrapers/twitter.py
+++ backend/scrapers/twitter.py
@@ -6,30 +6,30 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 import time
 
-URL = "https://twitter.com/LisaTalia"
 USERNAME = "bhaskarsqusre"
 PASSWORD = "scrapekaruisaccountse123@"
 
-driver = webdriver.Firefox()
-driver.get("https://twitter.com/i/flow/login")
-wait = WebDriverWait(driver, 30)
-email_field = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input")))
-email_field.send_keys(USERNAME)
-email_field.send_keys(Keys.RETURN)
-pass_field = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="password"]')))
-pass_field.send_keys(PASSWORD)
-pass_field.send_keys(Keys.RETURN)
+def twitter_scraper(URL: str) -> set[str]:
+    driver = webdriver.Firefox()
+    driver.get("https://twitter.com/i/flow/login")
+    wait = WebDriverWait(driver, 30)
+    email_field = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, "input")))
+    email_field.send_keys(USERNAME)
+    email_field.send_keys(Keys.RETURN)
+    pass_field = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, 'input[name="password"]')))
+    pass_field.send_keys(PASSWORD)
+    pass_field.send_keys(Keys.RETURN)
 
-time.sleep(5)
-driver.get(URL)
-time.sleep(5)
+    time.sleep(5)
+    driver.get(URL)
+    time.sleep(5)
 
-texts = set()
-for _ in range(5):
-    driver.find_element(By.TAG_NAME, "html").send_keys(Keys.END)
-    time.sleep(2)
-    res = driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="tweetText"]')
-    texts.update(stripped for el in res if (stripped:=el.text.strip()))
+    texts = set()
+    for _ in range(5):
+        driver.find_element(By.TAG_NAME, "html").send_keys(Keys.END)
+        time.sleep(2)
+        res = driver.find_elements(By.CSS_SELECTOR, 'div[data-testid="tweetText"]')
+        texts.update(stripped for el in res if (stripped:=el.text.strip()))
 
-print(texts)
-driver.close()
+    driver.close()
+    return texts
